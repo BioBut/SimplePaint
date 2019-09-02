4579d27c526b881a7c8861210aa2aad5e735006d
Nikita Norov <syori@archlinux.localdomain>
2019-09-02 16:34:13 +0300

    Added VerticalSeekBar class

---
+++ app/src/main/java/space/fstudio/simplepaint/Views/VerticalSeekBar.java
@@ -0,0 +1,14 @@
+package space.fstudio.simplepaint.Views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+@SuppressLint("AppCompatCustomView")
+public class VerticalSeekBar extends SeekBar {
+    public VerticalSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //TODO VERTICAL ROTATION
+    }
+}
