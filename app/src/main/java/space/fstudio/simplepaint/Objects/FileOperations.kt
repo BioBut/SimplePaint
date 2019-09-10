975fabeaa2613b2b7b15aca3dc82a7cca354b9af
Astru43 <45196326+Astru43@users.noreply.github.com>
2019-09-10 15:31:56 +0300

    Added save dialog

--- app/src/main/java/space/fstudio/simplepaint/Objects/FileOperations.kt
+++ app/src/main/java/space/fstudio/simplepaint/Objects/FileOperations.kt
@@ -1,9 +1,12 @@
 package space.fstudio.simplepaint.Objects
 
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Environment
+import android.widget.EditText
+import space.fstudio.simplepaint.R
 import java.io.File
 import java.io.FileOutputStream
 
@@ -13,20 +16,7 @@ class FileOperations {
     private lateinit var bMap: Bitmap
     fun saveBitmap(bitmap: Bitmap, context: Context) {
         bMap = Bitmap.createBitmap(bitmap)
-        lateinit var file: File
-        try {
-            if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
-                file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "filename.png")
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        tr = Thread(Runnable {
-            val oStream = FileOutputStream(file)
-            bMap.compress(Bitmap.CompressFormat.PNG, 10, oStream)
-            oStream.close()
-        })
-        tr.start()
+        saveDialog(context)
     }
 
     fun loadBitmap(context: Context): Bitmap? {
@@ -39,5 +29,52 @@ class FileOperations {
             bMap = BitmapFactory.decodeFile(file.path, opts)
         } else return null
         return bMap
+    }
+
+    private fun saveDialog(context: Context) {
+        val filename = EditText(context)
+        val builder = AlertDialog.Builder(context)
+        builder.setView(filename)
+        builder.setNegativeButton("Cancel") { _, _ -> }
+        builder.setPositiveButton("Save") { _, _ ->
+
+            lateinit var file: File
+            try {
+                if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+                    file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), filename.text.toString() + ".png")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            if (file.exists()) {
+                val builder2 = AlertDialog.Builder(context)
+                builder2.setTitle("File already exists!")
+                builder2.setMessage("Do you want to overwrite it?")
+                builder2.setNegativeButton("Cancel") { _, _ -> }
+                builder2.setPositiveButton("Overwrite") { _, _ ->
+                    tr = Thread(Runnable {
+                        val oStream = FileOutputStream(file)
+                        bMap.compress(Bitmap.CompressFormat.PNG, 10, oStream)
+                        oStream.close()
+                    })
+                    tr.start()
+                }
+                builder2.create().show()
+            } else {
+                tr = Thread(Runnable {
+                    val oStream = FileOutputStream(file)
+                    bMap.compress(Bitmap.CompressFormat.PNG, 10, oStream)
+                    oStream.close()
+                })
+                tr.start()
+            }
+
+        }
+        builder.setTitle("Save")
+        builder.setMessage("Give a filename")
+        builder.setIcon(R.drawable.ic_save_black_24dp)
+
+        builder.create().show()
     }
 }
